5caa8fb15d21ebfe08c8d932c11e6f71f3b7d925
Olivier Renault <monoliv@gmail.com>
2018-01-14 15:10:13 +0000

    fixing script

--- docker-files/connect-sftp/copy-files.py
+++ docker-files/connect-sftp/copy-files.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
 
+import logging
 import paramiko
 import hashlib
 import datetime
-import ConfigParser
+import configparser
+import sys
+import os
+
+conf_file = '/mnt/conf/sftp.conf'
+#conf_file = '/home/orenault/Developments/airflow-demo/docker-files/connect-sftp/sftp-local.conf'
 
 def read_conf(confFile):
-  sftpConf = {}
-  try:
-    with open(confFile, 'r') as conf:
-      config = ConfigParser.ConfigParser()
-      config.readfp(conf)
-      for section_name in config.sections():
-        for name, value in config.items(section_name):
-          sftpConf[name] = value
-    print
-  except IOError:
-		print ("Can't read conf file!")
-  return sftpConf
+    sftpConf = {}
+    try:
+        with open(confFile, 'r') as conf:
+            config = configparser.ConfigParser()
+            config.readfp(conf)
+            for section_name in config.sections():
+                for name, value in config.items(section_name):
+                    sftpConf[name] = value
+            print
+    except IOError:
+        print ("ERROR: Can't read conf file!")
+        sys.exit(0)
+    return sftpConf
 
 
 def sha256_checksum(filename, block_size=65536):
@@ -28,27 +35,27 @@ def sha256_checksum(filename, block_size=65536):
     return sha256.hexdigest()
 
 def main():
-    conf = read_conf('/mnt/conf/sftp.conf')
+    conf = read_conf(conf_file)
     # initialize paramiko client
     ssh = paramiko.SSHClient()
     ssh.load_host_keys(conf['known_hosts_file'])
-
     with open(conf['dest_path'] + '/sha256', "a+") as f:
         # initiate connection
         try:
             ssh.connect(conf['hostname'], username=conf['username'], key_filename=conf['ssh_key'], compress=True, look_for_keys=False)
             sftp = ssh.open_sftp()
             sftp.chdir(conf['src_path'])
-            #filelist = sftp.listdir()
             for filename in sftp.listdir():
                 try:
-                    print(sftp.stat(conf['dest_path'] + "/" + filename))
-                    print('file exists')
+                    local_file_size = os.stat(conf['dest_path'] + "/" + filename).st_size
+                    if local_file_size != sftp.stat(filename).st_size:
+                        raise IOError
                 except IOError:
-                    print('copying file')
                     sftp.get(filename, conf['dest_path'] + "/" + filename)
                     f.write(filename + " " + sha256_checksum(conf['dest_path'] + '/' + filename) + " " + str(datetime.datetime.now()).split('.')[0] + "\n")
+                    print(filename)
             ssh.close()
+            print('DONE')
         except paramiko.SSHException:
             print('Connection Error')
 
